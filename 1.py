481fe7d069fa08c10cc922678775f4c0bcccfba4
DenysDenysenko0 <148973568+DenysDenysenko0@users.noreply.github.com>
2025-03-10 11:16:02 +0200

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,32 @@
+# Функція для обчислення z
+def calczi(m):
+    return (m ** 0.5) + 10
+
+# Функція для знаходження середнього арифметичного всіх парних чисел від x до y
+def avgsimple(x, y):
+    suma = 0
+    count = 0
+    for num in range(x, y + 1):
+        if num % 2 == 0:
+            suma += num
+            count += 1
+
+    # Перевірка чи є парні числа
+    if count == 0:
+        return 0
+    
+    return suma / count
+
+# Введення значень користувачем
+m = int(input("Введіть m: "))
+x = int(input("Введіть x: "))
+y = int(input("Введіть y: "))
+
+# Виклик функцій і вивід результатів обчислень
+print("Результат z = ", calczi(m))
+
+avgnum = avgsimple(x, y)
+if avgnum is not 0:
+    print("Середнє арифметичне парних чисел = ", avgnum)
+else:
+    print("Парних чисел не виявлено, середнє арифметичне не можливо порахувати")
